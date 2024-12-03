1168677323234ac7b200842d7d7dd45b5d596d87
Matthieu Guerra <m.guerra@ra2.tech>
2024-12-03 16:05:18 +0100

    fix: install

--- start.sh
+++ start.sh
@@ -115,7 +115,7 @@ then
         exit 1
     fi
 fi
-pnpm install
+
 # Check if tsx is installed globally
 GLOBAL_DEPS=$(pnpm list -g --depth 0 --json)
 HAS_TSX=$(echo $GLOBAL_DEPS | jq '.[] | .dependencies | .tsx')
